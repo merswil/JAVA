b7827476a1ad7e1c0cce72c98b06a5914fa43f83
Mokete Merswil <moketemerswil01@gmail.com>
2026-08-23 02:50:15 +0300

    Main.java update

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-
-    void main() {
-        IO.print(String.format("Hello and welcome!\n"));
-        System.out.println("I love pizza");
-        System.out.println("It's very good!");
-    }
-}
---
+++ src/_01_Main.java
@@ -0,0 +1,19 @@
+public class _01_Main {
+
+    void main() {
+
+        // printing output in strings
+        IO.print(String.format("Hello and welcome!\n")); // The "\n" takes us to a new line
+        System.out.println("I love pizza");
+        System.out.println("It's very good!");
+
+        // This is a comment
+
+        /*
+        *This
+        * is
+        * a
+        * comment
+        * */
+    }
+}
